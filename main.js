df6aee67306c9c39cb24d01fa87610b34adad2fa
smriti <smratibatra24@gmail.com>
2024-09-20 00:55:36 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  let menu= document.querySelector('#menu-icon');
+  let navbar= document.querySelector('.navbar');
+
+  menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+  }
